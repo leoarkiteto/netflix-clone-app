39f674dfb38b5d77ea33c48659f21b86c00f3df3
Leo Franca <leoarkiteto@gmail.com>
2023-07-31 13:00:54 +0100

    :rotating_light: Setting up Eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/eslint-config-airbnb-with-typescript',
+		'plugin:prettier/recommended'
+	],
+	overrides: [
+		{
+			env: {
+				node: true
+			},
+			files: [
+				'.eslintrc.{js,cjs}'
+			],
+			parserOptions: {
+				sourceType: 'script',
+				project: './tsconfig.json'
+			}
+		}
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module'
+	},
+	plugins: [
+		'@typescript-eslint',
+		'vue'
+	],
+	rules: {}
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	trailingComma: "es5",
+	bracketSameLine: true,
+	arrowParens: "avoid",
+	vueIndentScriptAndStyle: true,
+	singleAttributePerLine: true,
+	plugins: ["prettier-plugin-tailwindcss"]
+};
